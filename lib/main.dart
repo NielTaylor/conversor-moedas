e9481fe45a759cbbc605b1b15993afe67686cfd9
NielTaylor <coreytaylor4556@gmail.com>
2023-07-18 22:25:48 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'telaInicial.dart';
+
+void main() async {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        labelStyle: TextStyle(color: Colors.amber),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.amber,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+    home: TelaInicial(),
+  ));
+}
+
+
