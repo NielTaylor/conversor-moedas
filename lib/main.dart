a046a5469507585644662d97e8c7010e4452565d
NielTaylor <coreytaylor4556@gmail.com>
2023-07-18 23:31:45 -0300

    restringido os campos de digitação dos valores das moedas apenas para carecteres númericos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'telaInicial.dart';
+import 'tela_inicial.dart';
 
 void main() async {
   runApp(MaterialApp(
